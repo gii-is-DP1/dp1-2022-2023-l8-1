1097cc25f6224fbfbb4bf4bc2573aaaedbf72df2
Jesus <jesusperike@gmail.com>
2022-11-13 20:02:11 +0100

    admins creation player added

---
+++ src/main/java/org/springframework/samples/notimeforheroes/player/PlayerService.java
@@ -0,0 +1,31 @@
+package org.springframework.samples.notimeforheroes.player;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerService {
+    
+
+    private PlayerRepository repo;
+
+    @Autowired
+    public PlayerService(PlayerRepository repository){
+        this.repo = repository;
+    }
+
+    public Collection<Player> findPlayers(){
+        return repo.findAll();
+    }
+
+
+    @Transactional
+    public void savePlayer(Player player){
+        repo.save(player);
+
+    }
+}
